2efb0f845c0f70c0a05bfbfcd3a50a3e0a69a9a5
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2023-10-19 16:16:24 +0200

    Make simple add-on that shows the number of cards

--- src/anki_convo/__init__.py
+++ src/anki_convo/__init__.py
@@ -1,3 +1,12 @@
 # SPDX-FileCopyrightText: 2023-present Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from anki_convo.card_count_view import init_card_count_view
+
+
+def init_addon():
+    init_card_count_view()
+
+
+init_addon()
