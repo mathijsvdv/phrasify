f6a9951b1f12f2f6bba20b0eb220deb3e7fde2ee
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2023-10-29 18:44:54 +0100

    Clean up comments

--- src/anki_convo/__init__.py
+++ src/anki_convo/__init__.py
@@ -8,25 +8,6 @@ from .addition_filter import init_addition_filter
 from .card_count_view import init_card_count_view
 from .field_filter import init_field_filter
 
-# If we want to support async requests, we need the following additional dependencies:
-# aiohttp, aiosignal, async-timeout, charset-normalizer, frozenlist, multidict, yarl
-
-# import importlib
-# openai_dependencies = [
-#     "aiohttp", "aiosignal", "async_timeout", "charset_normalizer", "frozenlist",
-#     "multidict", "yarl", "tqdm", "requests"
-# ]
-
-
-# for dependency in openai_dependencies:
-#     try:
-#         importlib.import_module(dependency)
-#     except ModuleNotFoundError as e:
-#         print(f"Could not find openai dependency `{dependency}`")
-#         print(e)
-#     else:
-#         print(f"Successfully imported dependency `{dependency}`!")
-
 
 def init_addon():
     init_openai()
@@ -36,16 +17,3 @@ def init_addon():
 
 
 init_addon()
-
-
-# import openai
-# completion = openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts "
-#                                   "with creative flair."},
-#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-#   ]
-# )
-
-# print(completion.choices[0].message)
