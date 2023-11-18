d9f1dc02c870cb60e4024b82eed0f0719b8caf5c
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2023-11-18 13:28:25 +0100

    Add possibility of using Ollama in the app

--- src/anki_convo/factory.py
+++ src/anki_convo/factory.py
@@ -3,6 +3,7 @@
 These functions are used to parse user input.
 """
 
+import re
 from functools import lru_cache
 from pathlib import Path
 from typing import Optional
@@ -11,6 +12,7 @@ from .card import CardSide
 from .chains.llm_input import LLMInputChain
 from .chains.remote import RemoteChain
 from .config import config
+from .llms.ollama import Ollama
 from .llms.openai import OpenAI
 
 __all__ = ["get_llm", "get_prompt", "get_card_side", "get_chain"]
@@ -30,6 +32,10 @@ def get_llm(llm_name: Optional[str] = None):
 
     if llm_name.startswith("gpt-"):
         return OpenAI(llm_name)
+    elif llm_name.startswith("ollama-"):
+        ollama_re = re.compile(r"ollama-(?P<ollama_name>.*)")
+        ollama_name = ollama_re.match(llm_name).group("ollama_name")
+        return Ollama(ollama_name)
     else:
         msg = f"Invalid LLM name: {llm_name}"
         raise ValueError(msg)
---
+++ src/anki_convo/llms/ollama.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Any
+
+import requests
+
+from .base import LLM
+
+
+@dataclass
+class Ollama(LLM):
+    """LLM that uses OpenAI's API."""
+
+    model: str = "mistral"
+    url: str = "http://localhost:11434"
+
+    @property
+    def endpoint(self) -> str:
+        return f"{self.url}/api/generate"
+
+    def _call(self, prompt: str, **kwargs: Any) -> str:  # noqa: ARG002
+        """Run the LLM on the given prompt and input."""
+        data = {"prompt": prompt, "model": self.model, "stream": False}
+
+        response = requests.post(self.endpoint, json=data, timeout=30)
+        response.raise_for_status()
+        response_str = response.json()["response"]
+        return response_str
