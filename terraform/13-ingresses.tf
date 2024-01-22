af9ce1e33f7cc778a02e3b68499d653588eba335
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2024-01-22 09:52:35 +0100

    docs: add documentation of the `null_resource` `ingresses`

--- terraform/13-ingresses.tf
+++ terraform/13-ingresses.tf
@@ -1,4 +1,19 @@
 resource "null_resource" "ingresses" {
+  /*
+  This resource is used to trigger the deletion of all ingresses
+  when the cluster is destroyed. This is necessary because the
+  ingress controller and external-dns resources are not managed
+  by Terraform. Namely:
+  - The AWS Load Balancer Controller creates a load balancer for each ingress. Upon
+    deletion of the ingress, the load balancer is deleted automatically, but only
+    if the AWS Load Balancer Controller is present. This resource ensures that the
+    ingresses are deleted before the AWS Load Balancer Controller is destroyed, so
+    it can handle the deletion of the load balancers.
+  - External DNS creates DNS records for each ingress. Upon deletion of the ingress,
+    the DNS records are deleted automatically, but only if the External DNS is present.
+    With this resource, we can ensure that the DNS records are deleted when the
+    cluster is destroyed.
+  */
   triggers = {
     eks_context = aws_eks_cluster.cluster.arn
   }
