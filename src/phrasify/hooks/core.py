88375a3d810825b3c9025fc9cdcb9f8859d8fefb
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2024-01-29 16:59:25 +0100

    refactor: rename LLMFilter to PhrasifyFilter, etc.
    This applies to PhrasifyFilter, PhrasifyFilterConfig, and also affects how the filter is called: the filter uses the 'phrasify' prefix now.

--- src/phrasify/hooks/core.py
+++ src/phrasify/hooks/core.py
@@ -1,6 +1,6 @@
-from .llm_filter import init_llm_filter
+from .phrasify_filter import init_phrasify_filter
 
 
 def init_hooks():
     """Initialize the hooks."""
-    init_llm_filter()
+    init_phrasify_filter()
--- src/phrasify/hooks/phrasify_filter.py
+++ src/phrasify/hooks/phrasify_filter.py
@@ -48,8 +48,8 @@ class LanguageFieldNames:
 
 
 @dataclass(frozen=True)
-class LLMFilterConfig:
-    """Configuration for the LLMFilter."""
+class PhrasifyFilterConfig:
+    """Configuration for the PhrasifyFilter."""
 
     card_generator: CardGeneratorConfig
     language_field_names: LanguageFieldNames
@@ -58,7 +58,7 @@ class LLMFilterConfig:
 class HasNote(Protocol):
     """Protocol for an object that has a note (`anki.template.TemplateRenderContext`)
 
-    This protocol has been introduced to more loosely couple the LLMFilter to Anki
+    This protocol has been introduced to more loosely couple the PhrasifyFilter to Anki
     and make it easier to test.
     """
 
@@ -71,7 +71,7 @@ class HasNote(Protocol):
 
 
 @dataclass
-class LLMFilter:
+class PhrasifyFilter:
     """Filter that generates a new card using an LLM and replaces the given field with
     the generated one."""
 
@@ -84,7 +84,7 @@ class LLMFilter:
     def __call__(self, field_text: str, field_name: str, context: HasNote) -> str:
         if field_text == f"({field_name})":
             # It's just the example text for the field, make a placeholder
-            return f"(llm filter applied to '{field_name}' field)"
+            return f"(phrasify filter applied to '{field_name}' field)"
 
         input_card = self.language_field_names.create_card(context.note())
 
@@ -114,8 +114,8 @@ class LLMFilter:
             )
             if field_text.strip() == "":
                 return (
-                    f"(llm filter was applied to an empty field. The LLM will be more "
-                    f"effective at generating cards if both "
+                    f"(phrasify filter was applied to an empty field. The LLM will be "
+                    f"more effective at generating cards if both "
                     f"the '{self.language_field_names.source}' field and "
                     f"the '{self.language_field_names.target}' field are filled with "
                     f"words in the source and target languages, respectively.)"
@@ -125,26 +125,26 @@ class LLMFilter:
         return new_field_text
 
 
-def create_llm_filter(
-    config: LLMFilterConfig,
+def create_phrasify_filter(
+    config: PhrasifyFilterConfig,
     card_generator_factory: CardGeneratorFactory | None = None,
 ):
-    """Create an LLMFilter from the config."""
+    """Create an PhrasifyFilter from the config."""
     if card_generator_factory is None:
         card_generator_factory = create_card_generator
 
     card_generator = card_generator_factory(config.card_generator)
-    return LLMFilter(
+    return PhrasifyFilter(
         card_generator=card_generator, language_field_names=config.language_field_names
     )
 
 
-def parse_llm_filter_name(filter_name: str) -> LLMFilterConfig:
+def parse_phrasify_filter_name(filter_name: str) -> PhrasifyFilterConfig:
     # TODO Make source_lang and target_lang optional by considering the config and
     # possibly the name of the deck that we're in.
 
     pattern = (
-        r"llm (?P<prompt_name>[a-zA-Z0-9_-]+) "
+        r"phrasify (?P<prompt_name>[a-zA-Z0-9_-]+) "
         r"source_lang=(?P<source_language>[a-zA-Z0-9_-]+) "
         r"target_lang=(?P<target_language>[a-zA-Z0-9_-]+) "
         r"source_field=(?P<source_field_name>[a-zA-Z0-9_-]+) "
@@ -171,14 +171,14 @@ def parse_llm_filter_name(filter_name: str) -> LLMFilterConfig:
         source=source_field_name, target=target_field_name
     )
 
-    llm_filter_config = LLMFilterConfig(
+    phrasify_filter_config = PhrasifyFilterConfig(
         card_generator=card_generator_config, language_field_names=language_field_names
     )
 
-    return llm_filter_config
+    return phrasify_filter_config
 
 
-def llm_filter(
+def phrasify_filter(
     field_text: str,
     field_name: str,
     filter_name: str,
@@ -188,7 +188,7 @@ def llm_filter(
     """Filter that generates the front or back of a language card from the front text
 
     It can be activated by a filter name like:
-    {{llm <prompt> source_lang=<source_lang> target_lang=<target_lang> source_field=<source_field> target_field=<target_field>:<field>}}
+    {{phrasify <prompt> source_lang=<source_lang> target_lang=<target_lang> source_field=<source_field> target_field=<target_field>:<field>}}
 
     Here:
     - <prompt> is the name of the prompt
@@ -202,8 +202,8 @@ def llm_filter(
 
     For example, suppose we have a card with fields Front='friend' (English)
     and Back='друг' (Ukrainian). If we replace {{Front}} and {{Back}} with the filters
-    {{llm vocab-to-sentence source_lang=English target_lang=Ukrainian source_field=Front target_field=Back:Front}}
-    {{llm vocab-to-sentence source_lang=English target_lang=Ukrainian source_field=Front target_field=Back:Back}}
+    {{phrasify vocab-to-sentence source_lang=English target_lang=Ukrainian source_field=Front target_field=Back:Front}}
+    {{phrasify vocab-to-sentence source_lang=English target_lang=Ukrainian source_field=Front target_field=Back:Back}}
     respectively, then:
     - The Front field is replaced with a sentence like: "She has many friends."
     - The Back field is replaced with a sentence like: "У неї багато друзів."
@@ -212,12 +212,12 @@ def llm_filter(
     -----
     If the LLM fails to generate a card, the field text is returned unchanged.
     """  # noqa: E501, RUF002
-    if not filter_name.startswith("llm"):
+    if not filter_name.startswith("phrasify"):
         # not our filter, return string unchanged
         return field_text
 
     try:
-        filter_config = parse_llm_filter_name(filter_name)
+        filter_config = parse_phrasify_filter_name(filter_name)
     except ValueError:
         return invalid_name(filter_name)
 
@@ -227,7 +227,7 @@ def llm_filter(
     card_generator_factory = cached2_card_generator_factory(
         id(context), card_generator_factory
     )
-    filt = create_llm_filter(
+    filt = create_phrasify_filter(
         filter_config, card_generator_factory=card_generator_factory
     )
 
@@ -238,8 +238,8 @@ def invalid_name(filter_name: str) -> str:
     return f"(Invalid filter name: {filter_name})"
 
 
-def init_llm_filter():
+def init_phrasify_filter():
     from anki import hooks
 
     # register our function to be called when the hook fires
-    hooks.field_filter.append(llm_filter)
+    hooks.field_filter.append(phrasify_filter)
