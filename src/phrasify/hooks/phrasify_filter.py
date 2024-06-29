87097a74ec728d3fa4907f3d7fa9d01e307e2d77
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2024-06-29 11:26:47 +0200

    fix: clean up unnecessary classes from previous LLMFilter

--- src/phrasify/hooks/phrasify_filter.py
+++ src/phrasify/hooks/phrasify_filter.py
@@ -1,29 +1,15 @@
-import json
 import re
-from collections import deque
 from dataclasses import dataclass
-from functools import lru_cache
-from typing import Any, Callable, Deque, Dict, Iterable, List, Optional, Mapping
-
-from anki import hooks
-from anki.template import TemplateRenderContext
+from typing import Mapping, Protocol
 
 from ..card import TranslationCard
-from ..chains.base import Chain
-from ..constants import GENERATED_CARDS_DIR
-from ..error import CardGenerationError, ChainError
-from ..factory import get_card_side, get_chain, get_llm_name, get_prompt
 from ..card_gen import (
     CardFactory,
     CardFactoryCreator,
-    CardGenerator,
     CardGeneratorConfig,
-    CardGeneratorFactory,
-    cached2_card_generator_factory,
-    create_card_generator,
+    cached2_card_factory_creator,
     create_card_factory,
 )
-from ..error import CardGenerationError
 from ..logging import get_logger
 
 # Possible hooks and filters to use
@@ -62,32 +48,6 @@ class LanguageFieldNames:
             raise ValueError(message)
 
 
-def parse_text_card_response(response: Dict[str, str]) -> List[TextCard]:
-    """Parse the response from an LLM into a list of TextCard objects"""
-    # TODO If using CSV, need to be robust to:
-    #   - Multiple sentences
-    #   - Commas in the sentences
-    #   - Quotes vs no quotes
-    #   - Headers vs no headers
-    #   - Newlines in the sentences?
-    #   - Multiple paragraphs
-    #   - Spaces around the sentences.
-    #   - ', ' vs ',' delimiter
-
-    # TODO We can also start with JSON, which is a bit easier to parse.
-    # But the response may be longer and require more tokens (more expensive).
-    # TODO In any case, probably want to parse the response into a list of
-    # dictionaries first, then convert each to cards.
-    response = response["result"]["text"]
-    card_dicts = json.loads(response)
-
-    return [TextCard.from_dict(card_dict) for card_dict in card_dicts]
-
-
-CardGenerator = Callable[[str], Iterable[TextCard]]
-CardFactory = Callable[[str], TextCard]
-
-
 @dataclass(frozen=True)
 class PhrasifyFilterConfig:
     """Configuration for the PhrasifyFilter."""
@@ -109,200 +69,6 @@ class HasNote(Protocol):
         A note is a mapping of field names to field values for a card.
         """
         ...
-
-
-@dataclass
-class ChainCardGenerator(CardGenerator):
-    """Can be called to generate language cards from an LLMInputChain."""
-
-    chain: Chain[Dict[str, Any], str]
-    llm: str
-    prompt: str
-    prompt_inputs: Dict[str, Any]
-
-    @property
-    def n_cards(self) -> int:
-        return self.prompt_inputs.get("n_cards", 1)
-
-    def get_chain_inputs(self, field_text: str) -> Dict[str, Any]:
-        return {
-            "llm": self.llm,  # TODO Should this be a string or an LLM object?
-            "prompt": self.prompt,
-            "prompt_inputs": dict(**self.prompt_inputs, field_text=field_text),
-        }
-
-    def __call__(self, field_text: str) -> List[TextCard]:
-        """Generate multiple language cards from the front text inserted into a
-        prompt."""
-        logger.debug(
-            f"{self.__class__.__name__} generating {self.n_cards} cards "
-            f"from field text {field_text!r},"
-            f"using chain {self.chain} with llm {self.llm!r}. "
-        )
-        if self.n_cards == 0:
-            return []
-
-        chain_inputs = self.get_chain_inputs(field_text)
-        try:
-            response = self.chain(chain_inputs)
-        except ChainError as e:
-            msg = f"Error generating card using chain inputs: {chain_inputs}"
-            raise CardGenerationError(msg) from e
-
-        cards = parse_text_card_response(response)
-
-        return cards
-
-
-class TextCardEncoder(json.JSONEncoder):
-    """JSON encoder for TextCard objects."""
-
-    def default(self, o: Any) -> Any:
-        if isinstance(o, TextCard):
-            return o.to_dict()
-        return super().default(o)
-
-
-class JSONCachedCardGenerator(CardGenerator):
-    """Can be called to generate language cards. Cache the result as a JSON."""
-
-    def __init__(
-        self, card_generator: CardGenerator, min_cards: int = 5, name: str = "default"
-    ):
-        self.card_generator = card_generator
-        self.min_cards = min_cards
-        self.name = name
-
-    def get_cache_path(self, field_text: str) -> str:
-        """Get the path to the cache file."""
-        # TODO Need to put the card ID in the cache path, so that we can have
-        # different cards with the same field_text.
-
-        return GENERATED_CARDS_DIR / f"{self.name}_{field_text}.json"
-
-    def get_from_cache(self, field_text: str) -> Deque[TextCard]:
-        """Get the cards from the cache, if they exist."""
-        cache_path = self.get_cache_path(field_text)
-        try:
-            with open(cache_path) as f:
-                cards = json.load(f, object_hook=TextCard.from_dict)
-
-        except FileNotFoundError:
-            cards = []
-
-        return deque(cards)
-
-    def write_to_cache(self, field_text: str, cards: Iterable[TextCard]):
-        """Write the cards to the cache."""
-        cache_path = self.get_cache_path(field_text)
-        cache_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(cache_path, "w") as f:
-            json.dump(list(cards), f, cls=TextCardEncoder)
-
-    def __call__(self, field_text: str) -> List[TextCard]:
-        """Generate language cards from the front text inserted into a prompt."""
-        cards = self.get_from_cache(field_text)
-        logger.debug(
-            f"Retrieving {len(cards)} cards from cache for field text {field_text!r}"
-        )
-
-        while True:
-            if len(cards) < self.min_cards:
-                logger.debug(
-                    f"Cache has {len(cards)} < {self.min_cards} cards, "
-                    f"generating more for field text {field_text!r}"
-                )
-                new_cards = self.card_generator(field_text)
-                cards.extend(new_cards)
-                logger.debug(
-                    f"Extended cache with {len(new_cards)} new cards "
-                    f"for field text {field_text!r}"
-                )
-
-            card = cards.popleft()
-            self.write_to_cache(field_text, cards)
-            yield card
-
-
-class NextCardFactory(CardFactory):
-    """Can be called to take the next language card from a card generator."""
-
-    def __init__(self, card_generator: CardGenerator):
-        self.card_generator = card_generator
-        self._card_iterator = None
-
-    def __call__(self, field_text: str) -> TextCard:
-        """Take next language card from the front text inserted into a prompt."""
-
-        if self._card_iterator is None:
-            try:
-                self._card_iterator = iter(self.card_generator(field_text))
-            except CardGenerationError as e:
-                logger.error(f"Error in card generator: {e}")
-                self._card_iterator = iter([])
-
-        try:
-            card = next(self._card_iterator)
-        except StopIteration:
-            card = TextCard(front=field_text, back=field_text)
-            logger.warning(f"No cards generated. Creating placeholder card {card}")
-
-        return card
-
-
-def create_card_factory(config: CardGeneratorConfig):
-    """Create a CardGenerator from the config."""
-    llm_name = get_llm_name()
-    prompt = get_prompt(config.prompt_name)
-    chain = get_chain()
-
-    prompt_inputs = {"n_cards": 10}
-    prompt_inputs.update(
-        {"lang_front": config.lang_front, "lang_back": config.lang_back}
-    )
-
-    name = f"{config.prompt_name}_{config.lang_front}_{config.lang_back}"
-    card_generator = ChainCardGenerator(
-        chain=chain, llm=llm_name, prompt=prompt, prompt_inputs=prompt_inputs
-    )
-    card_generator = JSONCachedCardGenerator(card_generator, name=name)
-
-    card_factory = NextCardFactory(card_generator)
-    card_factory = lru_cache(maxsize=None)(card_factory)
-    return card_factory
-
-
-CardGeneratorFactory = Callable[[CardGeneratorConfig], CardGenerator]
-CardFactoryCreator = Callable[[CardGeneratorConfig], CardFactory]
-
-
-@dataclass(frozen=True)
-class LLMFilterConfig:
-    """Configuration for the LLMFilter."""
-
-    card_generator: CardGeneratorConfig
-    card_side: str
-
-
-@dataclass
-class LLMFilter:
-    """Filter that generates the front or back of a language card from the front text
-    inserted into a prompt."""
-
-    def __init__(self, card_factory: CardFactory, card_side: CardSide):
-        self.card_factory = card_factory
-        self.card_side = card_side
-
-    def __call__(self, field_text: str, field_name: str) -> str:
-        if field_text == f"({field_name})":
-            # It's just the example text for the field, make a placeholder
-            return (
-                f"<llm filter applied to '{field_text}' field "
-                f"(f{self.card_side.value} side)>"
-            )
-
-        card = self.card_factory(field_text)
-        return getattr(card, self.card_side.value)
 
 
 @dataclass
@@ -356,32 +122,6 @@ def create_phrasify_filter(
     )
 
 
-def create_llm_filter(
-    config: LLMFilterConfig,
-    card_factory_creator: Optional[CardFactoryCreator] = None,
-):
-    """Create an LLMFilter from the config."""
-    if card_factory_creator is None:
-        card_factory_creator = create_card_factory
-
-    card_factory = card_factory_creator(config.card_generator)
-    card_side = get_card_side(config.card_side)
-    return LLMFilter(card_factory=card_factory, card_side=card_side)
-
-
-def create_llm_filter(
-    config: LLMFilterConfig,
-    card_factory_creator: Optional[CardFactoryCreator] = None,
-):
-    """Create an LLMFilter from the config."""
-    if card_factory_creator is None:
-        card_factory_creator = create_card_factory
-
-    card_factory = card_factory_creator(config.card_generator)
-    card_side = get_card_side(config.card_side)
-    return LLMFilter(card_factory=card_factory, card_side=card_side)
-
-
 def parse_phrasify_filter_name(filter_name: str) -> PhrasifyFilterConfig:
     # TODO Make source_lang and target_lang optional by considering the config and
     # possibly the name of the deck that we're in.
@@ -419,51 +159,6 @@ def parse_phrasify_filter_name(filter_name: str) -> PhrasifyFilterConfig:
     )
 
     return phrasify_filter_config
-
-
-def parse_llm_filter_name(filter_name: str) -> LLMFilterConfig:
-    # TODO Make lang_front and lang_back optional by considering the config and possibly
-    #  the name of the deck that we're in.
-
-    pattern = (
-        r"llm (?P<prompt_name>[a-zA-Z0-9_-]+) "
-        r"lang_front=(?P<lang_front>[a-zA-Z]+) "
-        r"lang_back=(?P<lang_back>[a-zA-Z]+) "
-        r"(?P<card_side>(front|back|f|b))"
-    )
-    match = re.match(pattern, filter_name)
-
-    if match:
-        prompt_name = match.group("prompt_name")
-        lang_front = match.group("lang_front")
-        lang_back = match.group("lang_back")
-        card_side = match.group("card_side")
-    else:
-        msg = f"Invalid filter name: '{filter_name}'"
-        raise ValueError(msg)
-
-    card_generator_config = CardGeneratorConfig(
-        prompt_name=prompt_name, lang_front=lang_front, lang_back=lang_back
-    )
-    llm_filter_config = LLMFilterConfig(
-        card_generator=card_generator_config, card_side=card_side
-    )
-
-    return llm_filter_config
-
-
-class CachedCardFactoryCreator:
-    """Create a CardGenerator from the config. Cache the result."""
-
-    def __init__(self, context_id: int):
-        self.context_id = context_id
-        self._call = lru_cache(maxsize=None)(create_card_factory)
-
-    def __call__(self, config: CardGeneratorConfig) -> CardFactory:
-        return self._call(config)
-
-
-cached2_card_factory_creator = lru_cache(maxsize=1)(CachedCardFactoryCreator)
 
 
 def phrasify_filter(
@@ -532,54 +227,3 @@ def init_phrasify_filter():
 
     # register our function to be called when the hook fires
     hooks.field_filter.append(phrasify_filter)
-
-
-def llm_filter(
-    field_text: str,
-    field_name: str,
-    filter_name: str,
-    context: TemplateRenderContext,
-) -> str:
-    """Filter that generates the front or back of a language card from the front text
-
-    It can be activated by a filter name like:
-    {{llm <prompt_name> lang_front=<lang_front> lang_back=<lang_back> <card_side>:<field_name>}}
-
-    Here:
-    - <prompt_name> is the name of the prompt
-    - <lang_front> and <lang_back> are the languages to use for the front and back of
-      the card
-    - <card_side> refers to the side ('front' or 'back') that will be generated. Use
-      'front' to replace the field with the generated front of the card. Use 'back' to
-      replace the field with the generated back of the card.
-    - <field_name> refers to the field name, e.g. 'Front' or 'Back' for basic cards.
-
-    For example, to generate sentences in English (front) and Ukrainian (back) from
-    the 'Front' field, use:
-    {{llm vocab-to-sentence lang_front=English lang_back=Ukrainian front:Front}}
-    for the front of the card, and:
-    {{llm vocab-to-sentence lang_front=English lang_back=Ukrainian back:Front}}
-    for the back of the card.
-    """  # noqa: E501
-    if not filter_name.startswith("llm"):
-        # not our filter, return string unchanged
-        return field_text
-
-    try:
-        filter_config = parse_llm_filter_name(filter_name)
-    except ValueError:
-        return invalid_name(filter_name)
-
-    card_factory_creator = cached2_card_factory_creator(context_id=id(context))
-    filt = create_llm_filter(filter_config, card_factory_creator=card_factory_creator)
-
-    return filt(field_text=field_text, field_name=field_name)
-
-
-def invalid_name(filter_name: str) -> str:
-    return f"Invalid filter name: {filter_name}"
-
-
-def init_llm_filter():
-    # register our function to be called when the hook fires
-    hooks.field_filter.append(llm_filter)
