7c3513f90496cad97476d4812c9dd4f10458cd50
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2024-07-13 14:27:11 +0200

    feat: add `init_event_loop()` to start the event loop upon loading the plugin

--- src/phrasify/__init__.py
+++ src/phrasify/__init__.py
@@ -4,6 +4,7 @@
 from . import _paths  # noqa: I001, F401
 from .__about__ import NAME, __version__  # noqa: F401
 
+import asyncio
 import os
 
 from .env import load_env
@@ -12,6 +13,10 @@ from .logging import setup_logging, get_logger
 
 setup_logging()
 logger = get_logger(__name__)
+
+
+def init_event_loop():
+    asyncio.get_event_loop()
 
 
 def init_package():
@@ -29,6 +34,7 @@ def init_addon():  # pragma: no cover
     init_hooks()
 
 
+init_event_loop()
 init_package()
 if os.getenv("INIT_PHRASIFY_ADDON", "true").lower() == "true":  # pragma: no cover
     init_addon()
